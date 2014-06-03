0eb8003f49c37260cbb222f55d81eec9854fb794
badbeef <badbeef@users.noreply.github.com>
2014-06-03 16:51:04 -0700

    Update fe.c
    Updated copyright to include date of original

--- fe.c
+++ fe.c
@@ -2,7 +2,7 @@
 
 The MIT License (MIT)
 
-Copyright (c) 2014 Chui-Tin Yen
+Copyright (c) 1993, 2014 Chui-Tin Yen
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
 */
 
 #include <stdlib.h>
